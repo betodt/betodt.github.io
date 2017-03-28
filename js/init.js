10290ee62aa0ba5f710cc9ac4c32b7a014d0d2a6
betodt <alberto.diaz.tostado@gmail.com>
2017-03-27 21:15:09 -0700

    navbar + layout

--- js/init.js
+++ js/init.js
@@ -1,9 +1,17 @@
 (function($){
   $(function(){
-
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+    });
     // Initialize collapsible (uncomment the line below if you use the dropdown variation)
     $('.collapsible').collapsible();
+
+    $.winSizeSpy();
+
+    $('.scrollspy').scrollSpy({
+        scrollOffset: window.innerHeight * 0.3
+    });
 
     $('.dropdown-button').dropdown({
         inDuration: 300,
@@ -13,8 +21,34 @@
         gutter: 0, // Spacing from edge
         belowOrigin: false, // Displays dropdown below the button
         alignment: 'left' // Displays dropdown with edge aligned to the left of button
-      }
-    );
+    });
+
+    // hover handler for sidebar
+    $('header ul.side-nav li:not(:first-child):not(:last-child)').hover(function() {
+        $(this).addClass('light-blue lighten-2');
+        $('a', this).removeClass('black-text');
+        $('a', this).addClass('white-text');
+    }, function() {
+        $(this).removeClass('light-blue lighten-2');
+        $('a', this).removeClass('white-text');
+        $('a', this).addClass('black-text');
+    });
+
+    // window on scroll
+    $(window).scroll(function(event) {
+        var scrollHeight = $(window).scrollTop();
+
+        var elmHeight = $('#index-banner').offset().top;
+
+        var elmSize = $('#index-banner').outerHeight();
+
+        var navHeight = $('header nav').height();
+
+        if (Math.abs(elmHeight - scrollHeight) > elmSize - navHeight) {
+            $('#index-banner').addClass('hide');
+            $('header div.hide').removeClass('hide').addClass('navbar-fixed');
+        }
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
